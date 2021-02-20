3d250bd3d174b58e6bda3de35fa36e381d03478d
Dhadhazi <dhadhazi@gmail.com>
2021-02-20 09:35:54 +0100

    Fix styled components SSR

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export const Nav = () => (
   <nav>
-    <Link href="products">Products</Link>
+    <Link href="/products">Products</Link>
     <Link href="/sell">sell</Link>
     <Link href="/orders">orders</Link>
     <Link href="/account">account</Link>
