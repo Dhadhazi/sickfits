beb7d55ada0969bba364d9f70bbd8cde4d07f2dc
Dhadhazi <dhadhazi@gmail.com>
2021-02-21 14:00:34 +0100

    File input and form styles

--- backend/.keystone/admin/.next/static/chunks/webpack.js
+++ backend/.keystone/admin/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "68dc6c4dcb7a8899ea55";
+/******/ 	var hotCurrentHash = "04cd8611b232fe05154b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/lib/useForm.js
+++ frontend/lib/useForm.js
@@ -1,12 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export default function useForm(initial = {}) {
   const [inputs, setInputs] = useState(initial);
-  const initialValues = Object.values(initial).join('');
-
-  useEffect(() => {
-    setInputs(initial);
-  }, [initialValues]);
 
   function handleChange(e) {
     let { value, name, type } = e.target;
