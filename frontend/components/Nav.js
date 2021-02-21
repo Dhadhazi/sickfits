692a7690cc545f559a799dc02956e7beb9b42ba5
Dhadhazi <dhadhazi@gmail.com>
2021-02-21 11:15:57 +0100

    Nav styles addded

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,10 +1,11 @@
 import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
 
 export const Nav = () => (
-  <nav>
+  <NavStyles>
     <Link href="/products">Products</Link>
     <Link href="/sell">sell</Link>
     <Link href="/orders">orders</Link>
     <Link href="/account">account</Link>
-  </nav>
+  </NavStyles>
 );
